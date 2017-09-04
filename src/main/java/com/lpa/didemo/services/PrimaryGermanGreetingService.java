0a27e7c4088c6c71390c32bdd51134f92a5d6fae
L.P <parisot.laurent@gmail.com>
2017-09-04 16:17:15 +0200

    default profile

--- src/main/java/com/lpa/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/lpa/didemo/services/PrimaryGermanGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"default", "en"})
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Primärer Grußdienst";
     }
 }
